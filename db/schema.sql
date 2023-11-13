0f250fb20dd182e1f405dc70155a4dad817c9e4d
gtorresv <giselle.torres15@gmail.com>
2023-11-12 22:58:29 -0500

    added missing code to schema and completed seeds

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,23 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     department_name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id 
-    title VARCHAR(30)
-    salary DECIMAL
-    department_id INT
+    id INT PRIMARY KEY auto_increment,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES department(id)
 )
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY auto_increment,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT
-    manager_id INT
+    role_id INT,
+    manager_id INT,
+    CONSTRAINT fk_role FOREIGN KEY(role_id) REFERENCES ROLE (id)
 )
---
+++ db/seeds.sql
@@ -0,0 +1,33 @@
+INSERT INTO department (department_name)
+VALUES 
+('Management'), 
+('Sales'),
+('Engineering'),
+('IT Department'),
+('Finance'),
+('Administration');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+('Manager', 80000, 1),
+('Salesperson', 75000, 2),
+('Computer Engineer', 95000, 3),
+('Office Administrator', 72000, 6),
+('Systems Analyst', 69000, 4),
+('IT Specialist', 76000, 4),
+('Accountant', 78000, 5),
+('IT Manager', 68000, 1),
+('Software Engineer' 110000, 3);
+
+
+INSERT INTO employee (first_name, last_name, manager_id, role_id)
+VALUES
+('Laura', 'Banks', 0, 1),
+('Tom', 'Smith', 1, 2),
+('Jerry', 'Smith', 1, 3),
+('Ava', 'Jones', 1, 4),
+('Noah', 'Davis', 8, 5),
+('Isabella', 'Lopez', 8, 6),
+('Sophia', 'Anderson', 1, 7),
+('Liam', 'Swift', 0, 8),
+('Maite', 'Casas', 1, 9);
